d208f3e7a8424c9dacdaa9f4fdf307cf3654c83d
Dimo Dinev <ddinev@vocera.com>
2018-10-03 06:22:26 -0400

    First git commit

---
+++ testing/Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Dimo Dinev on 2018-10-03.
+//  Copyright © 2018 Dimo Dinev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
